979076c583040a349cc3c569e16dec724a76caae
18BCS4336 <42218190+18BCS4336@users.noreply.github.com>
2018-11-21 01:09:26 +0530

    Add files via upload

---
+++ bee_project__final.ino
@@ -0,0 +1,22 @@
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,INPUT);
+pinMode(3,OUTPUT);
+}
+
+void loop() {
+ int x=digitalRead(2);
+    if(x==HIGH)
+  {
+    digitalWrite(3,HIGH);
+    delay(100);
+    digitalWrite(3,LOW);
+    delay (100);
+  }
+  digitalWrite(3,LOW);
+  // put your main code here, to run repeatedly:
+
+}
